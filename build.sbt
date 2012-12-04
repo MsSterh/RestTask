9f574b5a321b755d8068e073608ec14f3ef45e2f
Vlad Pranskevichus <vlprans@gmail.com>
2012-12-04 17:44:02 +0200

    Implement value storage

--- build.sbt
+++ build.sbt
@@ -20,6 +20,7 @@ libraryDependencies ++= {
     "net.liftweb"       %% "lift-webkit"        % liftVersion        % "compile",
     "net.liftweb"       %% "lift-mongodb-record"% liftVersion,
     "net.databinder.dispatch" %% "dispatch-core" % "0.9.4",
+    "redis.clients"     % "jedis" % "2.1.0",
     "org.eclipse.jetty" % "jetty-webapp"        % "8.1.7.v20120910"  % "container,test",
     "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container,test" artifacts Artifact("javax.servlet", "jar", "jar"),
     "ch.qos.logback"    % "logback-classic"     % "1.0.6"
--- src/main/scala/com/anahoret/resttask/api/RestService.scala
+++ src/main/scala/com/anahoret/resttask/api/RestService.scala
@@ -7,7 +7,7 @@ import util._
 import Helpers._
 import http.rest.RestHelper
 
-import json.JObject
+import json.{JObject, JValue}
 import json.JsonDSL._
 
 
@@ -15,6 +15,8 @@ object RestService extends RestHelper {
   serve {
     case "fib" :: AsInt(num) :: _ Get _ => ("response" -> fib(num)) : JObject
     case "google-body" :: _ Get _ => ("response" -> Google.responseSHA) : JObject
+    case "store" :: _ Post value  => Store.set(value.param("value").getOrElse("null")) : JValue
+    case "store" :: _ Get _ => ("response" -> Store.get) : JObject
   }
 
   private def fib(n: Int) = {
@@ -39,6 +41,15 @@ object RestService extends RestHelper {
     def responseBody: String = http(url("http://www.google.com") OK as.String)()
 
     def responseSHA: String = msgDigest.digest(responseBody.getBytes).map("%02x".format(_)).reduce(_ + _)
+  }
+
+  private object Store {
+    import redis.clients.jedis._
+
+    lazy val db: Jedis = new Jedis("localhost")
+
+    def set(x: String) = db.set("value", x)
+    def get: String = db.get("value")
   }
 
 }
